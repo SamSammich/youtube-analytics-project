afdf5e9d9472dfb627e823961ede8ffdd5c40f5e
Sam(RYZEN-PC) <abdusamadov55@gamil.com>
2023-04-09 18:15:47 +0900

    Done homework 5 (Test coverage 100%)

--- src/playlist.py
+++ src/playlist.py
@@ -1,23 +1,61 @@
+import datetime
 import os
-
+import isodate
 from googleapiclient.discovery import build
 
+
 class PlayList:
-    api_key: str = os.getenv('API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
+    API_KEY: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, playlist_id):
-        self.playlist_id = playlist_id
-        self.video_info = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                     id=playlist_id).execute()
-        self.video_title = self.video_info["items"][0]["snippet"]['title']
-        self.video_url = "https://www.youtube.com/channel/" + f"{playlist_id}"
-        self.video_view = self.video_info['items'][0]['statistics']['viewCount']
-        self.video_like = self.video_info['items'][0]['statistics']['likeCount']
-        self.video_comment = self.video_info['items'][0]['statistics']['commentCount']
+        self.__playlist_id = playlist_id
+        self.__playlists_info = self.youtube.playlists().list(id=playlist_id, part='contentDetails,snippet',
+                                                                maxResults=50, ).execute()
 
+        self.__each_video_info = self.youtube.playlistItems().list(playlistId=self.__playlist_id,
+                                                                     part='contentDetails', maxResults=50, ).execute()
+        self.__video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.__each_video_info['items']]
+
+        self.__video_response = self.youtube.videos().list(part='contentDetails, statistics', id=','.join(
+            self.__video_ids)).execute()
+
+        self.__title = self.__playlists_info['items'][0]['snippet']['title']
+        self.__url = f"https://www.youtube.com/playlist?list={self.__playlist_id}"
+
+    @property
+    def url(self):
+        """Returning url"""
+        return self.__url
+
+    @property
+    def title(self):
+        """Returning title"""
+        return self.__title
+
+    @property
     def total_duration(self):
-        pass
+        """Finding total duration"""
+        total_duration = datetime.timedelta()
+
+        for video in self.__video_response['items']:
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            total_duration += duration
+        return total_duration
 
     def show_best_video(self):
-        pass
+        """Finding best video """
+        max_like = 0
+        best_video = None
+        for i in range(len(self.__video_response['items'])):
+            like_count = int(self.__video_response['items'][i]['statistics']['likeCount'])
+            if like_count > max_like:
+                max_like = like_count
+                best_video = self.__video_response['items'][i]['id']
+        return f"https://youtu.be/{best_video}"
+
+
+
+pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+print(pl.show_best_video())
